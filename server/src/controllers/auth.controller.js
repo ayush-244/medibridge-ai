cdab9170f6f359ed311bc91493acfbda014e8a4f
Ayush <ayushku244@gmail.com>
2026-06-03 19:35:55 +0530

    JWT added

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
@@ -21,17 +23,24 @@ const registerUser = async (req, res) => {
       });
     }
 
+    // Hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       role,
     });
+
+    // Remove password from response
+    const userResponse = user.toObject();
+    delete userResponse.password;
 
     res.status(201).json({
       success: true,
       message: "User registered successfully",
-      data: user,
+      data: userResponse,
     });
   } catch (error) {
     console.error("Registration Error:", error.message);
@@ -44,6 +53,66 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -2,15 +2,18 @@ const express = require("express");
 
 const {
   registerUser,
+  loginUser,
 } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+
 router.get("/test", (req, res) => {
   res.json({
     success: true,
-    message: "Auth route working"
+    message: "Auth route working",
   });
 });
 
